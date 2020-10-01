2b7277f23479e4daddfefdff504fa25424dda800
unknown <dodongchien1232@gmail.com>
2020-10-01 12:04:56 +0700

    Completed assignment module 4

--- module4/harder/script.js
+++ module4/harder/script.js
@@ -5,13 +5,15 @@
   University:
   ----------- javascripts ------------------
 */
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-for (var i = 0; i < names.length; ++i) {
-  var firstLetter = names[i].charAt(0);
-  if (firstLetter.toLowerCase() === 'j') {
-    byeSpeaker.speak(names[i]);
-  } 
-  else {
-    helloSpeaker.speak(names[i]);
+(function () {
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  for (var i = 0; i < names.length; ++i) {
+    var firstLetter = names[i].charAt(0);
+    if (firstLetter.toLowerCase() === 'j') {
+      byeSpeaker.speak(names[i]);
+    } 
+    else {
+      helloSpeaker.speak(names[i]);
+    }
   }
-}
+}) ();
