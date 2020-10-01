ba74e858e6556f5a16e22d4b68651903f4f3f3d5
unknown <dodongchien1232@gmail.com>
2020-10-01 11:55:33 +0700

    Completed assignment module 4

--- module4/harder/script.js
+++ module4/harder/script.js
@@ -5,7 +5,7 @@
   University:
   ----------- javascripts ------------------
 */
-
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 for (var i = 0; i < names.length; ++i) {
   var firstLetter = names[i].charAt(0);
   if (firstLetter.toLowerCase() === 'j') {
